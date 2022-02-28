a1b31d008e8715a0d861b0485a914f9335cfa7d6
KindOfTea <delusionkind@gmail.com>
2022-03-01 01:13:55 +0300

    add start files and Romance numbers rules

---
+++ Convertation.go
@@ -0,0 +1,22 @@
+package convertation
+
+func convertation() {
+	//Check < 4000 - otherwise error or warning?
+
+	//Romance rules:
+	//
+	//I=1 V=5 X=10 L=50 C=100 D=500 M=1000
+	//
+	//Example : XLVII = XL (40 = 50-10) + V (5)  +II(2)
+	//
+	//!!!!number worthwhile on the right(that is from which it is deducated)
+	//Should be no more than what is multiplied by ten!!!
+	//----example:
+	// 99!=IC   99 ==XCIX
+	// 49!-IL   49 == LXIX
+	//
+	//!!!It is imposible to repeat 4 digits in a row. That is the number 40 cannot be recorded as XXXX, only as LX.
+	// Of all these rules, it follows that the max number that can be recorded by Roman numbers is MMMCMXCIX = 3999
+
+	//need counter of digits in a row
+}
